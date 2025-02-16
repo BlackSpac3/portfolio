43824ac71cb5fdb09add09d93682f06c3c1ee3db
BlackSpac3 <toyinacobs12@gmail.com>
2025-02-17 00:33:47 +0100

    Vercel Analytiucs Integration

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ActiveSectionProvider from "@/context/ActiveSectionProvider";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -48,6 +49,7 @@ export default function RootLayout({ children }) {
             <Footer />
           </ActiveSectionProvider>
         </AppThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
