16e23815bed300b7286050883d691584c8897d8e
BlackSpac3 <toyinacobs12@gmail.com>
2025-02-17 00:24:19 +0100

    Updated CV

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { Button } from "./ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
 
 const ThemeToggle = () => {
